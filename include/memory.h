7c44bbf8210efcf170b063968e2d55dac03439c5
idn7 <idn7@cornell.edu>
2025-05-17 19:40:07 -0400

    Defined memory regions and constraints

---
+++ include/memory.h
@@ -0,0 +1,32 @@
+// Memory region sizes
+#define MEMORY_SIZE 65536 // 64KB
+#define ROM_SIZE 8192 // 8KB
+#define RAM_SIZE 8192 // 8KB
+#define VIDEO_MEM_SIZE 2048 // 2KB
+#define SPRITE_MEM_SIZE 2048 // 2KB
+#define PALETTE_MEM_SIZE 256 // 256B
+#define SOUND_REG_SIZE 256 // 256B
+#define INPUT_REG_SIZE 256 // 256B
+#define SYSTEM_CTRL_SIZE 256 // 256B
+#define SYSTEM_ROM_SIZE 4096 // 4KB
+
+// Memory Map adressing constraints
+#define ROM_START 0x0000
+#define ROM_END 0x1FFF
+#define RAM_START 0x2000
+#define RAM_END 0x3FFF
+#define VIDEO_MEM_START 0x4000
+#define VIDEO_MEM_END 0x47FF
+#define SPRITE_MEM_START 0x4800
+#define SPRITE_MEM_END 0x4FFF
+#define PALETTE_MEM_START 0x5000
+#define PALETTE_MEM_END 0x50FF
+#define SOUND_REG_START 0x5100
+#define SOUND_REG_END 0x51FF
+#define INPUT_REG_START 0x5200
+#define INPUT_REG_END 0x52ff
+#define SYSTEM_CTRL_START 0x5300
+#define SYSTEM_CTRL_END 0x53FF
+#define SYSTEM_ROM_START 0x5400
+#define SYSTEM_ROM_END 0x63FF
+
