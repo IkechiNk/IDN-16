a519428971657b5b19a0dcdb4d57af95799ff5ba
idn7 <idn7@cornell.edu>
2025-06-07 02:19:38 -0400

    Fixed include errors

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
-#define CLAY_IMPLEMENTATION
-#include "clay.h"
-#include "renderers/SDL2/clay_renderer_SDL2.c"
-
 #include <SDL.h>
 #include <SDL_ttf.h>
+
+#define CLAY_IMPLEMENTATION
+#include "lib/clay/clay.h"
+#include "lib/clay/SDL2/clay_renderer_SDL2.c"
 
 int main(void) {
     printf("Hello CLAY!");
